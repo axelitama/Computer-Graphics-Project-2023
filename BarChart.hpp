9cc660a450f0141a0abc03579bb917797705073f
Alex Amati <alex.amati@mail.polimi.it>
2023-09-07 00:07:00 +0200

    Changed light dir

--- BarChart.hpp
+++ BarChart.hpp
@@ -699,7 +699,7 @@ void BarChart::updateUniformBuffer(uint32_t currentImage) {
 
     glm::mat4 View = glm::lookAt(camPos, camTarget, glm::vec3(0,1,0));
 
-    gubo.DlightDir = glm::normalize(glm::vec3(1, 3, 1));
+    gubo.DlightDir = glm::normalize(glm::vec3(2, 4, 1));
     gubo.DlightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
     gubo.AmbLightColor = glm::vec3(0.1f);
     gubo.eyePos = camPos;
